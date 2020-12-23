aa66d82cefbd58e4bb2d94c106e53634f376e79b
Y0hy0h <y0hy0h@gmx.net>
2020-12-23 13:07:33 +0100

    Add test for SortedDict

---
+++ tests/SortedDictTest.elm
@@ -0,0 +1,48 @@
+module SortedDictTest exposing (..)
+
+import Expect exposing (Expectation)
+import Fuzz
+import SortedDict exposing (SortedDict)
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe "SortedDict"
+        [ fuzz (Fuzz.list Fuzz.int) "keeps insertion order" <|
+            \list ->
+                let
+                    ( withIds, sortedDict ) =
+                        fromList list
+                in
+                SortedDict.toList sortedDict
+                    |> Expect.equal withIds
+        , fuzz (Fuzz.list Fuzz.int) "sorts items" <|
+            \list ->
+                let
+                    ( withIds, sortedDict ) =
+                        fromList list
+                in
+                sortedDict
+                    |> SortedDict.stableSortWith (\( _, item1 ) ( _, item2 ) -> compare item1 item2)
+                    |> SortedDict.toList
+                    |> List.map (\( _, item ) -> item)
+                    |> Expect.equal (List.sort list)
+        ]
+
+
+fromList : List Int -> ( List ( Int, Int ), SortedDict Int Int )
+fromList list =
+    let
+        withIds =
+            List.indexedMap (\id item -> ( id, item )) list
+
+        sortedDict =
+            List.foldl
+                (\( id, item ) sd ->
+                    SortedDict.insert id item sd
+                )
+                SortedDict.empty
+                withIds
+    in
+    ( withIds, sortedDict )
